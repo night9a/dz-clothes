732de98d44ba7d5a04c52db8cf2b095201f6fd08
night9a <nightxcros@gmail.com>
2026-02-11 07:01:20 +0100

    port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, proxy: { '/api': { target: 'https://dz-clothes-zyae.vercel.app', changeOrigin: true } } },
+  server: { port: 5000, proxy: { '/api': { target: 'https://dz-clothes-zyae.vercel.app', changeOrigin: true } } },
 })
