6703859fee211506af27e303a4f9bb7f929480b0
night9a <nightxcros@gmail.com>
2026-02-11 22:37:31 +0100

    try

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, proxy: { '/api': { target: 'https://dz-clothes-zyae.vercel.app', changeOrigin: true } } },
+  server: {
+    port: 5173,
+    host: true, // allow external access
+    allowedHosts: 'all', // allow ngrok domain
+    proxy: {
+      '/api': {
+        target: 'https://dz-clothes-zyae.vercel.app',
+        changeOrigin: true,
+      }
+    }
+  },
 })
